83e2cc5522c6322cc838fe41e756ced09b105df4
Wences81 <zhylinpochta@gmail.com>
2023-11-17 18:17:42 +0100

    17/11/2023

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 const button = document.querySelector('.btn');
 const image = document.querySelector('.img');
-const url = "https://api.thecatapi.com/v1/images/search";
+const url = "https://api.thecatapi.com/v1/images/BkIEhN3pG";
 
 async function fetchHandler() {
     try {
         const response = await fetch(url);
         const data = await response.json();
         console.log(data)
-        image.src = data.url
+        image.src = data.id;
     } catch (error) {
         console.log(error)
     }
